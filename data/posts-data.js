7aa12045eeca85ed31c0472b5cdaaeb3a0096a54
elilai <laihouxin@camera360.com>
2018-12-19 18:23:09 +0800

    2018年12月19日
    提交

---
+++ data/posts-data.js
@@ -0,0 +1,70 @@
+let local_database = [{
+    postId: 0,
+    date: '2018年12月18日',
+    title: '腾讯 omi-mp-create 发布',
+    imgSrc: '/images/post/tx.png',
+    content: 'omi-mp-create 和 omio 一样，使用了 omi packages 里的 obaa 监听数据变化自动更新视图。',
+    reading: '3452',
+    collection: '1087',
+    avatar: '/images/avatar/1.png',
+    headImgSrc: '/images/post/tx.png',
+    author: 'TX',
+    dateTime: '2018年12月19日',
+    detail: 'omi-mp-create 和 omio 一样，使用了 omi packages 里的 obaa 监听数据变化自动更新视图。和早期发布的 westore 对比的话，就是不用 diff 了，数据变更之后 setData 的最短路径自动就出来了，性能上一个档次。'
+}, {
+    postId: 1,
+    date: '2018年12月18日',
+    title: 'Taro：将已有微信小程序转换为多端应用',
+    imgSrc: '/images/post/Taro.png',
+    content: '在距离 Taro 1.1 发布 之后一个月，经历了 500 多次提交和 17 个预览版本的迭代之后，我们有信心在今天发布 Taro 1.2 正式版。',
+    reading: '5233',
+    collection: '2873',
+    avatar: '/images/avatar/2.png',
+    headImgSrc: '/images/post/Taro.png',
+    author: 'TX',
+    dateTime: '2018年12月18日',
+    detail: '在距离 Taro 1.1 发布 之后一个月，经历了 500 多次提交和 17 个预览版本的迭代之后，我们有信心在今天发布 Taro 1.2 正式版。Taro 1.2 比起 Taro 1.1 和其它的小程序开发框架，增加了许多首次在小程序框架上实现的功能'
+}, {
+    postId: 2,
+    date: '2018年12月17日',
+    title: '前端开发 2018 回顾及 2019 展望',
+    imgSrc: '/images/post/fe.png',
+    content: '前端发展的世界发展迅速。非常快。本文将回顾2018年最重要的前端新闻，值得关注的事件和 JavaScript 趋势。',
+    reading: '1234',
+    collection: '23',
+    avatar: '/images/avatar/3.png',
+    headImgSrc: '/images/post/fe.png',
+    author: 'FE',
+    dateTime: '2018年12月17日',
+    detail: 'WebAssembly 发布了主要版本，核心规范达到 1.0;从 NPM 下载量来看 React、jQuery、Angular 和 Vue 是 4 个最受欢迎的前端模块。请进一步阅读以了解前端库的最新发展;React 多年来一直主导 Web 开发，2018 年它根本没有放慢速度。根据 Stackoverflow 调查，它仍然是最受欢迎的库之一。'
+}, {
+    postId: 3,
+    date: '2018年12月18日',
+    title: '阳澄湖大闸蟹',
+    imgSrc: '/images/post/crab.png',
+    content: '阳澄湖是海水与淡水在长江交汇所遇到的第一个湖泊，水位常年稳定在2米左右，地形复杂、光照充足，湖内有着丰富的生物品种，湖边则是遍地稻谷，水草丰富、螺蛆更肥，独特的地理条件为大闸蟹的生长提供了良好的环境',
+    reading: '154',
+    collection: '87',
+    avatar: '/images/avatar/4.png',
+    headImgSrc: '/images/post/crab.png',
+    author: '吃货',
+    dateTime: '2018年12月17日',
+    detail: '阳澄湖大闸蟹，江苏省苏州市特产，中国国家地理标志产品，阳澄湖大闸蟹又名金爪蟹。产于苏州阳澄湖。蟹身不沾泥，俗称清水大闸蟹，体大膘肥，青壳白肚，金爪黄毛，肉质膏腻。农历9月的雌蟹、10月的雄蟹，性腺发育最佳。煮熟凝结，雌者成金黄色，雄者如白玉状，滋味鲜美。2005年5月9日，原国家质检总局批准对“阳澄湖大闸蟹”实施原产地域产品保护。 2018年9月25日报道，外地蟹被运到阳澄湖，贴标价翻十倍。 '
+}, {
+    postId: 4,
+    date: '2018年12月18日',
+    title: '猫有哪些超级萌的瞬间？',
+    imgSrc: '/images/post/cat.png',
+    content: '真的超可爱有次拿着猫粮不给她拖，她就发出呜呜呜声，还这样看着我',
+    reading: '123',
+    collection: '11',
+    avatar: '/images/avatar/5.png',
+    headImgSrc: '/images/post/cat.png',
+    author: '猫梓',
+    dateTime: '2018年12月18日',
+    detail: '真的超可爱有次拿着猫粮不给她拖，她就发出呜呜呜声，还这样看着我'
+}];
+
+module.exports = {
+    postList: local_database
+}
