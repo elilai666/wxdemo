bdcd95f148190bd6ae02df54179ac4db63ae3967
elilai <laihouxin@camera360.com>
2018-12-19 18:23:25 +0800

    2018年12月19日
    提交2

---
+++ pages/posts/post-detail/post-detail.js
@@ -0,0 +1,73 @@
+const postsData = require('../../../data/posts-data.js');
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let postId = options.id;
+        this.setData(postsData.postList[postId]);
+        // for (let post of postsData.postList) {
+        //     console.log(post.id);
+        //     if (post.postId == postId) {
+        //         this.setData(post);
+        //     }
+        // }
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -8,6 +8,17 @@ Page({
 
     },
     /**
+     * 点击文章跳转
+     */
+    onPostTap: function (e) {
+        ws:wx.navigateTo({
+            url: 'post-detail/post-detail?id=' + e.currentTarget.dataset.postId,
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+        })
+    },
+    /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
