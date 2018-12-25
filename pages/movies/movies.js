29d59f3ea869309ab845095582f2d34f7185ad4c
elilai <laihouxin@camera360.com>
2018-12-25 18:41:15 +0800

    补充提交

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -86,52 +86,15 @@ Page({
         this.setData(tmpObj);
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    onReachBottom: function(e) {
+        
     },
-
     /**
-     * 生命周期函数--监听页面显示
+     * "更多"点击事件
      */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    onMoreTap: function(e){
+        wx.navigateTo({
+            url: 'movie-grid/movie-grid-template.wxml'
+        })
     }
 })
